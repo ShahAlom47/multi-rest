bebb835264c020db5740475ea1352c9663278c74
ShahAlom47 <sahalom4729@gmail.com>
2025-10-25 21:59:06 +0600

    added dash  notification

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const AdminDashboardHome = () => {
     return (
         <div className=' space-y-3 max-w  w-full'>
+          
           AdminDashboardHome
         </div>
     );
---
+++ src/components/DashboardComponets/AdminNotification.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AdminNotification = () => {
+    return (
+        <div>
+            <h2>Admin Notifications</h2>
+            <ul>
+                <li>New user registration</li>
+                <li>System update available</li>
+                <li>Server downtime scheduled</li>
+            </ul>
+        </div>
+    );
+};
+
+export default AdminNotification;
