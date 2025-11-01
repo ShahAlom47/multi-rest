d9b218599bfc1b97a474020603a78017d7861fae
ShahAlom47 <sahalom4729@gmail.com>
2025-11-02 00:47:36 +0600

    adde   notification db  collection

--- src/lib/database/db_collections.ts
+++ src/lib/database/db_collections.ts
@@ -3,6 +3,7 @@ import { Collection, Db } from "mongodb";
 import clientPromise from "./db_connection";
 import { User } from "@/Interfaces/userInterfaces";
 import { TenantData } from "@/Interfaces/tenantInterface";
+import { NotificationData } from "@/Interfaces/NotificationInterface";
 
 
 // Define the User type (you can extend it as needed)
@@ -17,5 +18,10 @@ export const getTenantCollection = async (): Promise<Collection<TenantData>> => 
   const client = await clientPromise;
   const db: Db = client.db("MultiRest"); // Replace with your database name
   return db.collection<TenantData>("tenants");
+};
+export const getNotificationCollection = async (): Promise<Collection<NotificationData>> => {
+  const client = await clientPromise;
+  const db: Db = client.db("MultiRest"); // Replace with your database name
+  return db.collection<NotificationData>("notifications");
 };
 
